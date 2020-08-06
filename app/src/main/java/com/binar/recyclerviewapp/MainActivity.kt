0207192ae4cea8bb08b86c1a05eed1572b2afe12
wisungyo <wisungyo@gmail.com>
2020-08-06 08:44:50 +0700

    finish

--- app/src/main/java/com/binar/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/binar/recyclerviewapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.binar.recyclerviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,11 +15,22 @@ class MainActivity : AppCompatActivity() {
         this.supportActionBar?.hide()
 
         val listOfPlayer = arrayListOf(
-            PlayerData("Ikhsan Lemon", "RRQ", "22"),
+            PlayerData("Lemon", "RRQ", "22"),
             PlayerData("Tuturu", "RRQ", "22"),
-            PlayerData("Ayam Jago", "RRQ", "22"),
+            PlayerData("AyamJG", "RRQ", "22"),
             PlayerData("Xinnn", "RRQ", "22"),
-            PlayerData("R7 Tatsumaki", "RRQ", "22"),
+            PlayerData("R7", "RRQ", "22"),
+            PlayerData("LJ", "RRQ", "22"),
+            PlayerData("V Y N", "RRQ", "22"),
+            PlayerData("Xoxo", "RRQ", "22"),
+            PlayerData("Albert", "RRQ", "22"),
+            PlayerData("T A K A", "RRQ", "22"),
+            PlayerData("Jamess", "RRQ", "22"),
+            PlayerData("Lemon", "RRQ", "22"),
+            PlayerData("Tuturu", "RRQ", "22"),
+            PlayerData("AyamJG", "RRQ", "22"),
+            PlayerData("Xinnn", "RRQ", "22"),
+            PlayerData("R7", "RRQ", "22"),
             PlayerData("LJ", "RRQ", "22"),
             PlayerData("V Y N", "RRQ", "22"),
             PlayerData("Xoxo", "RRQ", "22"),
@@ -31,12 +44,15 @@ class MainActivity : AppCompatActivity() {
         val linearLayout = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val gridLayout = GridLayoutManager(this, 3, LinearLayoutManager.VERTICAL, false)
 
-        if (radio_linear.isChecked) {
-            recyclerView.layoutManager = linearLayout
-        } else if (radio_grid.isChecked) {
-            recyclerView.layoutManager = gridLayout
+        sv_recyclerview.layoutManager = linearLayout
+
+        list_radio.setOnCheckedChangeListener { group, i ->
+            val radio: RadioButton = findViewById(i)
+
+            if (radio.id == radio_linear.id) sv_recyclerview.layoutManager = linearLayout
+            else if (radio.id == radio_grid.id) sv_recyclerview.layoutManager = gridLayout
         }
 
-        recyclerView.adapter = adapter
+        sv_recyclerview.adapter = adapter
     }
 }
